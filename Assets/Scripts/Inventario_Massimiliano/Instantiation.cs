187eb631ee46332a8151bfb62858979c482e7113
massipellegrino <max99.pellegrino@gmail.com>
2022-02-13 22:01:57 +0100

    Prove problemi animazione beccaccino

--- Assets/Scripts/Inventario_Massimiliano/Instantiation.cs
+++ Assets/Scripts/Inventario_Massimiliano/Instantiation.cs
@@ -39,7 +39,7 @@ public class Instantiation : MonoBehaviour
 
     private void Start()
     {
-        TentPosition = NPC.GetChild(2);
+        TentPosition = NPC.Find("TentPlace");
         FirePosition = TentPosition.GetChild(0);
 
     }
